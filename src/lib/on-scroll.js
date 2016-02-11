1da3ecf62ab34f2dfa8d5ab40e33099743784ca2
flames of love <mechanicofthesequence@gmail.com>
2016-02-11 03:51:05 +0100

    fix on-scroll

--- src/lib/on-scroll.js
+++ src/lib/on-scroll.js
@@ -3,7 +3,8 @@ import debounce from './lodash/function/debounce'
 export default function onScroll (el, percent_or_px, handler) {
   let _el = el && el.scrollHeight ? el : window
   let body = el && el.scrollHeight ? el : document.body
-  let throttled = debounce(_onScroll, 100, true)
+  let throttled = debounce(_onScroll, 100, {leading: true, trailing: true, maxWait: 200})
+  // let throttled = () => { console.log('scroll?'); _throttled.apply(this, arguments) }
   let timeout
 
   _el.addEventListener('scroll', throttled)
@@ -23,8 +24,9 @@ export default function onScroll (el, percent_or_px, handler) {
     if (obj.working) return timeout ? null : timeout = setTimeout(throttled, 10)
     let rect = body.getBoundingClientRect()
     let bottom_px = window.pageYOffset + window.innerHeight
-    
-    if (percent_or_px < 1 && rect.height * percent_or_px < bottom_px || rect.height - percent_or_px < bottom_px) {
+
+    // console.log(percent_or_px, rect.height - percent_or_px, bottom_px)
+    if ((percent_or_px < 1 && (rect.height * percent_or_px) < bottom_px) || (rect.height - percent_or_px) < bottom_px) {
       obj.working = true
       handler(function () {
         setTimeout(throttled, 10)
