87670efa8f4d09cf62c1fc7ff2d349e244bb9a86
flames of love <mechanicofthesequence@gmail.com>
2016-02-25 22:04:21 +0100

    remove blog

--- src/router.js
+++ src/router.js
@@ -16,7 +16,8 @@ import poll from './views/poll'
 import notifications from './views/notifications'
 import cpanel from './views/cpanel'
 import api_docs from './views/action-docs'
-import blog from './views/blog'
+// import blog from './views/blog'
+import rpi from './views/rpi'
 
 // these are the views which will change the body's class to view-### otherwise view-default
 let classView = {
@@ -28,7 +29,7 @@ let classView = {
   category,
   debate,
   poll,
-  blog,
+  // blog,
 }
 
 let router = new Router({ el: 'view' }, function (request) {
@@ -74,7 +75,10 @@ router.addRoute('/cpanel/:active?/:id?', cpanel)
 // api-docs (remove me)
 router.addRoute('/action-docs/:active?/:id?', api_docs)
 //blog
-router.addRoute('/blog', blog)
+// router.addRoute('/blog', blog)
+
+// rpi experiment
+router.addRoute('/rpi/:id?', rpi)
 
 // TODO remove me when the styles are fixed
 router.on('route', (route) => {
