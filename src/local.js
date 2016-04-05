4727a92df558e4566405668ad19fdb28eb6893f2
flames of love <mechanicofthesequence@gmail.com>
2016-04-05 23:59:43 +0200

    use es6 localforage

--- src/local.js
+++ src/local.js
@@ -1,13 +1,33 @@
 'use strict'
 
-import addGetItems from './lib/localForage/getItems'
-import addSetItems from './lib/localForage/setItems'
+import localforage from './lib/localForage/localforage'
+import { localforageGetItems, getItemsIndexedDB, getItemsWebsql, getItemsGeneric } from './lib/localForage/getItems'
+import { localforageSetItems, setItemsIndexedDB, setItemsWebsql, setItemsGeneric } from './lib/localForage/setItems'
 
-let localforage = require('localforage')
+var localforagePrototype = Object.getPrototypeOf(localforage)
 
-// add functions to prototype
-addGetItems(localforage)
-addSetItems(localforage)
+localforagePrototype.getItems = localforageGetItems
+localforagePrototype.getItems.indexedDB = function () {
+  return getItemsIndexedDB.apply(this, arguments)
+}
+localforagePrototype.getItems.websql = function () {
+  return getItemsWebsql.apply(this, arguments)
+}
+localforagePrototype.getItems.generic = function () {
+  return getItemsGeneric.apply(this, arguments)
+}
+
+localforagePrototype.setItems = localforageSetItems
+localforagePrototype.setItems.indexedDB = function () {
+  return setItemsIndexedDB.apply(this, arguments)
+}
+localforagePrototype.setItems.websql = function () {
+  return setItemsWebsql.apply(this, arguments)
+}
+localforagePrototype.setItems.generic = function () {
+  return setItemsGeneric.apply(this, arguments)
+}
+
 
 localforage.config({
   name: 'Affinaty',
