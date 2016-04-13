7de82cfedb16c627966e5f3c23c9691c3879f5ab
flames of love <mechanicofthesequence@gmail.com>
2016-04-13 13:00:54 +0200

    fix onresize

--- src/lib/dom/element-onresize.js
+++ src/lib/dom/element-onresize.js
@@ -20,6 +20,7 @@ function resizeListener (e) {
   if (win._raf) cancelFrame(win._raf)
   win._raf = requestFrame(function () {
     let trigger = win._trigger
+    if (!trigger) return
     trigger._listeners.forEach(function (fn) {
       fn.call(trigger, e)
     })
