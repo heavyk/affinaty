b929aea17a7c721a2375f3db7930f84c8a8d8dfb
flames of love <mechanicofthesequence@gmail.com>
2016-05-23 21:19:45 +0200

    get only latest categories

--- src/api/category.js
+++ src/api/category.js
@@ -4,7 +4,7 @@ import local from '../local'
 import assign from '../lib/lodash/object/assign'
 import isEqual from '../lib/lodash/lang/isEqual'
 import Ambition from '../lib/insightful/consciousness/ambition'
-import { insert_d } from '../lib/ordered-array'
+import { insert_d, remove_d } from '../lib/ordered-array'
 import { title_dsc } from '../lib/order-by'
 
 class category_ extends Ambition {
@@ -13,14 +13,14 @@ class category_ extends Ambition {
     this.situations = {
       '/': {
         '>' () {
-          this.skip = this['+created']
+          this.skip = this.t_created
         }
       }
     }
   }
 
   pregage () {
-    this['+created'] = this['-created'] = 0
+    this.t_created = 0
     this.list = []
     this.exists = {}
     this.skip = 0
@@ -33,9 +33,9 @@ class category_ extends Ambition {
 
     local.getItems(['category', 'category:+created', 'category:-created'], (err, data) => {
       if (data && data.category) {
+        if (data.t_created) this.t_created = data.t_created
         for (var i = 0; i < data.category.length; i++)
-          this.insert(data.category[i])
-        this.skip = this['+created']
+          this.insert(data.category[i], true)
         this.now('/')
       }
 
@@ -45,20 +45,23 @@ class category_ extends Ambition {
 
   insert (d, silent) {
     let loc = this.exists[d._id]
-    if (!this['+created'] || this['+created'] > d.created) this['+created'] = d.created
-    if (!this['-created'] || this['-created'] < d.created) this['-created'] = d.created
+    if (!this.t_created || this.t_created < d.created) this.t_created = d.created
     if (loc === void 0) {
       insert_d(d, this.list, this.exists, title_dsc)
       if (!silent) {
         this.emit(d._id, d)
+        console.log('inserted new category', d._id, d)
       }
     } else {
       let _d = this.list[loc]
       if (!isEqual(_d, d)) {
-        // make a copy
-        let __d = assign({}, _d)
-        assign(_d, d)
+        remove_d(d, this.list, this.exists, loc)
+        insert_d(d, this.list, this.exists, title_dsc)
+        // TODO: only emit if there are listeners
         if (!silent) {
+          // make a copy (for the event)
+          let __d = assign({}, _d)
+          console.log('updated category', d._id, d, __d)
           this.emit(d._id, d, __d)
         }
       }
@@ -74,25 +77,22 @@ class category_ extends Ambition {
     let query = this.query
     if (typeof api !== 'object') return setTimeout(() => { this.go(next) }, 10)
     // TODO change the queries to go based on time
-    api.action('category*', assign({skip: this.skip}, query), (data) => {
+    let skip = this.t_created
+    api.action('category*', assign({skip: skip}, query), (data) => {
       // if skip is really big, then we've already fetched
-      let silent = this.skip < 1321052100000
-      let gt_created = 0
-      let lt_created = 0
+      let silent = skip < 1321052100000
       for (var i = 0; i < data.length; i++)
         this.insert(data[i], silent)
 
-      // this.skip += data.length
       if (next) next()
       if (data.length === this.query.limit) setTimeout(() => this.go(), 100)
       else this.now('/')
 
-      api.local.setItems({
+      if (data.length) local.setItems({
         'category': this.list,
-        'category:+created': this['+created'],
-        'category:-created': this['-created'],
+        'category:+created': this.t_created,
       }, (err) => {
-        if (err) return
+        if (err) return console.error('could not save categories into local', err)
         else console.log('saved categories into local')
       })
     })
