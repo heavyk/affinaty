a9b3a583311feabe6c4819aed73482e1420bb69e
flames of love <mechanicofthesequence@gmail.com>
2016-06-08 06:21:50 +0200

    update ractive transform (import rcu-builders directly)

--- gobble-ractive.js
+++ gobble-ractive.js
@@ -1,18 +1,214 @@
+'use strict'
+
 var rcu = require('rcu')
-var builders = require('rcu-builders')
 var postcss = require('postcss')
+var toSource = require('tosource')
 var path = require('path')
 rcu.init(require('ractive'))
 
 module.exports = ractive
 
-function ractive (source, options) {
-	var builder = builders[ options.type || 'amd' ]
+// ---------
+// to_source
+// ---------
 
-	if (!builder) {
-		throw new Error('Cannot convert Ractive component to "' + options.type + '". Supported types: ' + Object.keys(builders))
+function to_fun_old (object, filter, indent, startingIndent) {
+	var seen = []
+	return walk(object, filter, indent === undefined ? '	' : (indent || ''), startingIndent || '', seen)
+
+	function walk (object, filter, indent, currentIndent, seen) {
+		var nextIndent = currentIndent + indent
+		object = filter ? filter(object) : object
+
+		switch (typeof object) {
+			case 'string':
+				return JSON.stringify(object)
+			case 'boolean':
+			case 'number':
+			case 'undefined':
+				return '' + object
+			case 'function':
+				return object.toString()
+		}
+
+		if (object === null) {
+			return 'null'
+		}
+		if (object instanceof RegExp) {
+			return stringifyRegExp(object)
+		}
+		if (object instanceof Date) {
+			return 'new Date(' + object.getTime() + ')'
+		}
+
+		var seenIndex = seen.indexOf(object) + 1
+		if (seenIndex > 0) {
+			return '{$circularReference:' + seenIndex + '}'
+		}
+		seen.push(object)
+
+		function join (elements) {
+			return indent.slice(1) + elements.join(',' + (indent && '\n') + nextIndent) + (indent ? ' ' : '')
+		}
+
+		if (Array.isArray(object)) {
+			return '[' + join(object.map(function (element) {
+				return walk(element, filter, indent, nextIndent, seen.slice())
+			})) + ']'
+		}
+		var keys = Object.keys(object)
+		return keys.length ? '{' + join(keys.map(function (key) {
+			return (legalKey(key) ? key : JSON.stringify(key)) + ':' + walk(object[key], filter, indent, nextIndent, seen.slice())
+		})) + '}' : '{}'
+	}
+}
+
+var KEYWORD_REGEXP = /^(abstract|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|undefined|var|void|volatile|while|with)$/
+
+function legalKey (string) {
+	return /^[a-z_$][0-9a-z_$]*$/gi.test(string) && !KEYWORD_REGEXP.test(string)
+}
+
+// Node.js 0.10 doesn't escape slashes in re.toString() or re.source
+// when they were not escaped initially.
+// Here we check if the workaround is needed once and for all,
+// then apply it only for non-escaped slashes.
+var isRegExpEscaped = (new RegExp('/')).source === '\\/'
+
+function stringifyRegExp (re) {
+	if (isRegExpEscaped) {
+		return re.toString()
+	}
+	var source = re.source.replace(/\//g, function (found, offset, str) {
+		if (offset === 0 || str[offset - 1] !== '\\') {
+			return '\\/'
+		}
+		return '/'
+	})
+	var flags = (re.global && 'g' || '') + (re.ignoreCase && 'i' || '') + (re.multiline && 'm' || '')
+	return '/' + source + '/' + flags
+}
+
+
+
+
+// -------
+// BUILDER
+// -------
+
+
+
+
+const requirePattern = /require\s*\(\s*(?:"([^"]+)"|'([^']+)')\s*\)/g
+
+function stringify (key) {
+	if (/^[a-zA-Z$_][a-zA-Z$_0-9]*$/.test(key)) {
+		return key
 	}
 
+	return JSON.stringify(key)
+}
+
+
+function builder (definition, options) {
+	options = options || {}
+	let outro = createOutro(definition)
+
+	let imports = [ `import Ractive from 'ractive';` ]
+	let script = definition.script
+	let counter = 0
+
+	if (definition.modules.length) {
+		definition.modules.forEach(path => {
+			imports.push(`import __import${counter}__ from '${path}';`)
+			script = script.replace(requirePattern, (str, _, p) => p === path ? `__import${counter}__` : str)
+			counter += 1
+		})
+	}
+
+	definition.imports.forEach(imported => {
+		let path = imported.href
+
+		if (!options.preserveExtensions) {
+			path = path.replace(/\.[a-zA-Z]+$/, '')
+		}
+
+		imports.push(`import __import${counter}__ from '${path}';`)
+		counter += 1
+	})
+
+	const importBlock = imports.join('\n')
+
+	const beforeScript = [
+		'"use strict"'.importBlock,
+		`// component: ${definition.name}`,
+		'var component = { exports: {} };'
+	].join('\n')
+
+	const code = [
+		beforeScript,
+		script,
+		outro,
+		'export default Ractive.extend( component.exports );'
+	].join('\n')
+
+	const map = options.sourceMap ?
+		rcu.generateSourceMap(definition, {
+			offset: beforeScript.split('\n').length,
+			hires: options.hires !== false,
+			file: options.sourceMapFile,
+			source: options.sourceMapSource,
+			content: definition.source
+		}) : null
+
+	return {code, map}
+}
+
+function to_fun (tpl, options) {
+	options = options || {}
+	for (var i in tpl) {
+		console.log(i, tpl[i])
+	}
+	return
+`function (node, options) {
+	//	yay!!!
+	return h()
+}`
+}
+
+// make ls file to convert the template
+// add the ls file to the watcher
+
+function createOutro (definition, indent) {
+	indent = indent || ''
+	const css = definition.css // ? new CleanCSS().minify(definition.css).styles : ''
+	const imports = definition.imports.map((imported, i) => `${stringify(imported.name)}: __import${definition.modules.length + i}__`)
+
+	let outro = [
+		`${indent}component.exports.template = ${toSource(definition.template, null, '	')};`,
+		// `${indent}component.exports.fun = ${to_fun(definition.template, null, '	')};`,
+	]
+
+
+	if (css) outro.push(`${indent}component.exports.css = ${toSource(css)};`)
+	if (imports.length) outro.push(`${indent}component.exports.components = { ${imports.join(', ')} };`)
+
+	return outro.join('\n')
+}
+
+
+
+
+
+// ----------------
+// GOBBLE TRANSFORM
+// ----------------
+
+
+
+
+function ractive (source, options) {
+	options = options || {}
 	options.sourceMap = options.sourceMap !== false
 
 	if (options.sourceMap) {
@@ -22,20 +218,7 @@ function ractive (source, options) {
 
 	var parsed = rcu.parse(source)
 	parsed.name = path.basename(this.src)
-	var dup = {}
-	for (var i = 0; i < parsed.modules.length; i++) {
-		var m = parsed.modules[i]
-		if (dup[m]) {
-			console.warn(' * WARN:', this.src)
-			console.warn(' * duplicate import:', m)
-			parsed.modules.splice(i--, 1)
-		}
-		dup[m] = true
-	}
 
-	// perhaps this should go instead into rcu-builders
-	// for now this is here, but soon, just use the custom postcss enabled rcu-builders.js
-	// https://github.com/ractivejs/rcu-builders#building
 	postcss(options.postcss).process(parsed.css).then(function (result) {
 		result.warnings().forEach(function (warn) {
 			console.warn(warn.toString())
--- gobblefile.js
+++ gobblefile.js
@@ -1,13 +1,77 @@
 'use strict'
-var gobble = require('gobble')
-var babel = require('rollup-plugin-babel')
-var path = require('path')
-var fs = require('fs')
-var crypto = require('crypto')
+const gobble = require('gobble')
+const babel = require('rollup-plugin-babel')
+const path = require('path')
+const fs = require('fs')
+const crypto = require('crypto')
 
-var FILES = [
+const FILES = [
 	'app.js',
 	'screen.css',
+]
+
+const EXTERNAL_LIBS = [
+	 'ractive',
+	 'ractive-transitions-fade',
+	 'moment',
+	 'lie',
+	 'crontabjs',
+	 'spin.js',
+	 'gemini-scrollbar',
+	 'masonry-layout',
+	 'dropzone', // this needs to be replaced by generic code
+	 'chart.js',
+	 'es6-map',
+	 'lie',
+	 'url',
+	 'querystring',
+	 'smart-app-banner',
+	 'engine.io-client',
+]
+
+const POSTCSS_PLUGINS = [
+	require('postcss-import'),
+	require('precss'),
+	require('postcss-color-function'),
+	require('autoprefixer-core', { browsers: ['last 2 versions'] }),
+].concat(gobble.env() === 'production' ? [
+	require('cssnano'),
+] : [])
+
+const BABEL_PLUGINS = [
+	[require('babel-plugin-external-helpers-2')],
+	[require('babel-plugin-transform-es2015-literals')],
+	[require('babel-plugin-transform-es2015-function-name')],
+	[require('babel-plugin-transform-es2015-arrow-functions')],
+	[require('babel-plugin-transform-es2015-block-scoped-functions')],
+	[require('babel-plugin-transform-es2015-object-super')],
+	[require('babel-plugin-transform-es2015-shorthand-properties')],
+	[require('babel-plugin-transform-es2015-sticky-regex')],
+	[require('babel-plugin-transform-es2015-unicode-regex')],
+	[require('babel-plugin-transform-es2015-constants')],
+	[require('babel-plugin-transform-es2015-parameters')],
+	[require('babel-plugin-transform-es2015-block-scoping')],
+	// [require('babel-plugin-transform-es2015-typeof-symbol')],
+	[require('babel-plugin-transform-es2015-template-literals'), { loose: true }],
+	[require('babel-plugin-transform-es2015-classes'), { loose: true }],
+	[require('babel-plugin-transform-es2015-computed-properties'), { loose: true }],
+	[require('babel-plugin-transform-es2015-for-of'), { loose: true }],
+	[require('babel-plugin-transform-es2015-spread'), { loose: true }],
+	[require('babel-plugin-transform-es2015-destructuring'), { loose: true }],
+	// [require('babel-plugin-transform-regenerator'), { async: false, asyncGenerators: false }],
+
+	// optional
+	[require('babel-plugin-transform-undefined-to-void')],
+	[require('babel-plugin-transform-strict-mode')],
+	// [require('babel-plugin-transform-es2015-duplicate-keys')],
+	// [require('babel-plugin-transform-minify-booleans')],
+	// [require('babel-plugin-transform-merge-sibling-variables')],
+	[require('babel-plugin-transform-member-expression-literals')],
+	[require('babel-plugin-transform-property-literals')],
+
+	// debugger
+	// [require('babel-plugin-transform-remove-console')],
+	// [require('babel-plugin-transform-remove-debugger')],
 ]
 
 gobble.cwd(__dirname)
@@ -16,77 +80,20 @@ var affinaty = gobble([
 	gobble('src')
 		.transform(require('./gobble-ractive.js'), {
 			type: 'es6',
-			postcss: [
-				require('postcss-import'),
-				require('precss'),
-				require('postcss-color-function'),
-				require('autoprefixer-core', { browsers: ['last 2 versions'] }),
-				require('cssnano'),
-			]
+			postcss: POSTCSS_PLUGINS,
 		})
 		.transform('rollup', {
 			entry: 'affinaty-web.js',
 			// entry: 'affinaty-cpanel.js',
 			dest: 'app.js',
 			format: 'cjs',
-			external: [
-				 'ractive',
-				 'ractive-transitions-fade',
-				 'moment',
-				 'lie',
-				 'crontabjs',
-				 'spin.js',
-				 'gemini-scrollbar',
-				 'masonry-layout',
-				 'dropzone', // this needs to be replaced by generic code
-				 'chart.js',
-				 'es6-map',
-				 'lie',
-				 'url',
-				 'querystring',
-				 'smart-app-banner',
-				 'engine.io-client',
-			],
+			external: EXTERNAL_LIBS,
 			plugins: [
 				babel({
 					// "presets": [ "es2015-rollup" ],
 					exclude: 'node_modules/**',
-					plugins: [
-						[require('babel-plugin-external-helpers-2')],
-						[require('babel-plugin-transform-es2015-literals')],
-						[require('babel-plugin-transform-es2015-function-name')],
-						[require('babel-plugin-transform-es2015-arrow-functions')],
-						[require('babel-plugin-transform-es2015-block-scoped-functions')],
-						[require('babel-plugin-transform-es2015-object-super')],
-						[require('babel-plugin-transform-es2015-shorthand-properties')],
-						[require('babel-plugin-transform-es2015-sticky-regex')],
-						[require('babel-plugin-transform-es2015-unicode-regex')],
-						[require('babel-plugin-transform-es2015-constants')],
-						[require('babel-plugin-transform-es2015-parameters')],
-						[require('babel-plugin-transform-es2015-block-scoping')],
-						// [require('babel-plugin-transform-es2015-typeof-symbol')],
-						[require('babel-plugin-transform-es2015-template-literals'), { loose: true }],
-						[require('babel-plugin-transform-es2015-classes'), { loose: true }],
-						[require('babel-plugin-transform-es2015-computed-properties'), { loose: true }],
-						[require('babel-plugin-transform-es2015-for-of'), { loose: true }],
-						[require('babel-plugin-transform-es2015-spread'), { loose: true }],
-						[require('babel-plugin-transform-es2015-destructuring'), { loose: true }],
-						// [require('babel-plugin-transform-regenerator'), { async: false, asyncGenerators: false }],
-
-						// optional
-						[require('babel-plugin-transform-undefined-to-void')],
-						[require('babel-plugin-transform-strict-mode')],
-						// [require('babel-plugin-transform-es2015-duplicate-keys')],
-						// [require('babel-plugin-transform-minify-booleans')],
-						// [require('babel-plugin-transform-merge-sibling-variables')],
-						[require('babel-plugin-transform-member-expression-literals')],
-						[require('babel-plugin-transform-property-literals')],
-
-						// debugger
-						// [require('babel-plugin-transform-remove-console')],
-						// [require('babel-plugin-transform-remove-debugger')],
-					],
-					"compact": false
+					plugins: BABEL_PLUGINS,
+					compact: false,
 				})
 			]
 		})
@@ -107,18 +114,11 @@ var affinaty = gobble([
 	, gobble('files/styles')
 		.transform('less', {
 	    src: 'screen.less',
-	    dest: 'screen.css'
+	    dest: 'screen.css',
 	  })
 		.transform('postcss', {
-			plugins: [
-				require('postcss-import'),
-				require('precss'),
-				require('postcss-color-function'),
-				require('autoprefixer-core', { browsers: ['last 2 versions'] }),
-			].concat(gobble.env() === 'production' ? [
-				require('cssnano'),
-			] : []),
-			src: 'screen.css'
+			plugins: POSTCSS_PLUGINS,
+			src: 'screen.css',
 		})
 		// .moveTo( 'css' )
 
