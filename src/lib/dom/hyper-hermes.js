627a485653a1cdfce4c9cb19c1da5dd51ccfbb01
flames of love <mechanicofthesequence@gmail.com>
2016-06-12 00:23:37 +0200

    improve hyper-hermes & add observable

--- src/lib/dom/hyper-hermes.js
+++ src/lib/dom/hyper-hermes.js
@@ -1,11 +1,13 @@
 import ClassList from './class-list.js'
-// var split = function (str) {
-//   return str ? String.prototype.split(str) : []
-// }
-import words from '../lodash/string/words'
-var document = window.document
+import each from '../lodash/collection/forEach'
+var doc = window.document
 
-function context () {
+/*
+TODO ITEMS:
+ * extract out the attribute setting function and make it available to the attribute observable so setting attributes will work properyly for shortcut syntax
+*/
+
+function context (createElement) {
 
   var cleanupFuncs = []
 
@@ -14,15 +16,13 @@ function context () {
     function item (l) {
       var r
       function parseClass (string) {
-        // var m = split(string, /([\.#]?[a-zA-Z0-9_:-]+)/)
-        var m = words(string)
-        if (/^\.|#/.test(m[1]))
-          e = document.createElement('div')
+        var m = string.split(/([\.#]?[a-zA-Z0-9_:-]+)/)
+        if (/^\.|#/.test(m[1])) e = createElement('div')
         forEach(m, function (v) {
-          var s = v.substring(1,v.length)
+          var s = v.substring(1, v.length)
           if (!v) return
           if (!e) {
-            e = document.createElement(v)
+            e = createElement(v)
           } else if (v[0] === '.') {
             ClassList(e).add(s)
           } else if (v[0] === '#') {
@@ -37,13 +37,13 @@ function context () {
         if (!e) {
           parseClass(l)
         } else {
-          e.appendChild(r = document.createTextNode(l))
+          e.appendChild(r = doc.createTextNode(l))
         }
       } else if ('number' === typeof l
         || 'boolean' === typeof l
         || l instanceof Date
         || l instanceof RegExp ) {
-          e.appendChild(r = document.createTextNode(l.toString()))
+          e.appendChild(r = doc.createTextNode(l.toString()))
       } else if (Array.isArray(l)) {
         forEach(l, item)
       } else if (isNode(l)) {
@@ -51,36 +51,45 @@ function context () {
       } else if (l instanceof window.Text) {
         e.appendChild(r = l)
       } else if ('object' === typeof l) {
-        for (var k in l) {
-          if ('function' === typeof l[k]) {
+        // for (var k in l) {
+        each(l, function (attr_val, k) {
+          if ('function' === typeof attr_val) {
             if (/^on\w+/.test(k)) {
-              if (e.addEventListener){
-                e.addEventListener(k.substring(2), l[k], false)
-                cleanupFuncs.push(function(){
-                  e.removeEventListener(k.substring(2), l[k], false)
+              if (e.addEventListener) {
+                e.addEventListener(k.substring(2), attr_val, false)
+                cleanupFuncs.push(function() {
+                  e.removeEventListener(k.substring(2), attr_val, false)
                 })
               } else {
-                e.attachEvent(k, l[k])
-                cleanupFuncs.push(function(){
-                  e.detachEvent(k, l[k])
+                e.attachEvent(k, attr_val)
+                cleanupFuncs.push(function() {
+                  e.detachEvent(k, attr_val)
                 })
               }
             } else {
               // observable
-              e[k] = l[k]()
-              cleanupFuncs.push(l[k](function (v) {
-                e[k] = v
+              // e[k] = attr_val()
+              e.setAttribute(k, attr_val())
+              cleanupFuncs.push(attr_val(function (v) {
+                // e[k] = v
+                e.setAttribute(k, v)
+                // console.log('set attribute', k, '->', v)
               }))
             }
           } else if (k === 'data') {
-            for(var s in l[k]) e.dataset[s] = l[k][s]
+            for(var s in attr_val) e.dataset[s] = attr_val[s]
           } else if (k === 'for') {
-            e.htmlFor = l[k]
+            e.htmlFor = attr_val
           } else if (k === 'c' || k === 'class') {
-            e.className = Array.isArray(l[k]) ? l[k].join(' ') : l[k]
+            // e.className = Array.isArray(attr_val) ? attr_val.join(' ') : attr_val
+            if (Array.isArray(attr_val)) {
+              forEach(attr_val, function (c) {
+                ClassList(e).add(c)
+              })
+            } else ClassList(e).add(attr_val)
           } else if (k === 'on') {
-            if ('object' === typeof l[k]) {
-              for (var s in l[k]) (function(event, listener) {
+            if ('object' === typeof attr_val) {
+              for (var s in attr_val) (function(event, listener) {
                 if ('function' === typeof listener) {
                   // ???
                   (e.on || e.addEventListener)
@@ -92,19 +101,19 @@ function context () {
                       .call(e, event, listener, false)
                   })
                 }
-              })(s, l[k][s])
-            // } else if ('function' === typeof l[k]) {
+              })(s, attr_val[s])
+            // } else if ('function' === typeof attr_val) {
             //   // another event listener ...
             //   // re-emit listened events into this listener...
-            //   // for(k in l[k]._listeners) { ... }
+            //   // for(k in attr_val._listeners) { ... }
             // } else {
             //   debugger
             }
           } else if (k === 's' || k === 'style') {
-            if ('string' === typeof l[k]) {
-              e.style.cssText = l[k]
+            if ('string' === typeof attr_val) {
+              e.style.cssText = attr_val
             } else {
-              for (var s in l[k]) (function(s, v) {
+              for (var s in attr_val) (function(s, v) {
                 if ('function' === typeof v) {
                   // observable
                   e.style.setProperty(s, v())
@@ -112,44 +121,63 @@ function context () {
                     e.style.setProperty(s, val)
                   }))
                 } else {
-                  e.style.setProperty(s, l[k][s])
+                  e.style.setProperty(s, attr_val[s])
                 }
-              })(s, l[k][s])
+              })(s, attr_val[s])
             }
           } else if (k.substr(0, 5) === "data-") {
-            e.setAttribute(k, l[k])
-          } else if (typeof l[k] !== 'undefined') {
+            e.setAttribute(k, attr_val)
+          } else if (typeof attr_val !== 'undefined') {
             // this is an ugly hack to be able to use holder.js
             // I'd like to perhaps move it out to a wrapper function
             // TODO: add composition hooks instead of this shit...
-            if (k === 'src' && e.tagName === 'IMG' && ~l[k].indexOf('holder.js')) {
-              e.dataset.src = l[k]
+            if (~k.indexOf(':')) {
+              var attr = k.split(':')
+              switch (attr[0]) {
+                case 'xlink':
+                  e.setAttributeNS('http://www.w3.org/1999/xlink', attr[1], attr_val)
+                  break
+                default:
+                  console.error('unknown namespaced attribute: ' + k)
+              }
+            } else
+            if (k === 'src' && e.tagName === 'IMG' && ~attr_val.indexOf('holder.js')) {
+              e.dataset.src = attr_val
               console.log('you are using holder ... fix this')
               // setTimeout(function () {
               //   require('holderjs').run({images: e})
               // },0)
             } else {
-              e[k] = l[k]
+              // e[k] = attr_val
+              e.setAttribute(k, attr_val)
             }
           }
-        }
+        })
       } else if ('function' === typeof l) {
-        //assume it's an observable!
-        var v = l.call(e)
+        var is_observable = l.name === 'observable'
+        var v = is_observable ? l.call(e) : l.call(this, e)
+        // console.log('v', l.length, l.name, v)
         e.appendChild(
           r = isNode(v) ? v
-            : Array.isArray(v) ? arrayFragment(e,v)
-            : document.createTextNode(v)
+            : Array.isArray(v) ? arrayFragment(e, v)
+            : doc.createTextNode(v)
         )
-
-        cleanupFuncs.push(l(function (v) {
-          if (isNode(v) && r.parentElement) {
-            r.parentElement.replaceChild(v, r), r = v
-          // TODO: observable-array cleanup
-          } else {
-            r.textContent = v
-          }
-        }))
+        // assume we want to make a scope...
+        // call the function and if it returns an element, or an array, appendChild
+        if (is_observable) {
+          // assume it's an observable!
+          // TODO: allow for an observable-array implementation
+          // if (typeof v === 'function') {
+            cleanupFuncs.push(l(function (v) {
+              if (isNode(v) && r.parentElement) {
+                r.parentElement.replaceChild(v, r), r = v
+              // TODO: observable-array cleanup
+              } else {
+                r.textContent = v
+              }
+            }))
+          // }
+        }
       }
 
       return r
@@ -162,18 +190,13 @@ function context () {
   }
 
   h.cleanup = function () {
-    for (var i = 0; i < cleanupFuncs.length; i++){
+    for (var i = 0; i < cleanupFuncs.length; i++) {
       cleanupFuncs[i]()
     }
   }
 
   return h
 }
-
-var h = module.exports = context()
-h.context = context
-
-export default h
 
 function isNode (el) {
   return el && el.nodeName && el.nodeType
@@ -194,10 +217,14 @@ function forEachReverse (arr, fn) {
 }
 
 function arrayFragment(e, arr) {
-  var frag = document.createDocumentFragment()
+  var frag = doc.createDocumentFragment()
   var first = e.childNodes.length
   forEach(arr, function(v) {
-    frag.appendChild(isNode(v) ? v : document.createTextNode(v))
+    frag.appendChild(
+      isNode(v) ? v
+        : Array.isArray(v) ? arrayFragment(frag, v)
+        : doc.createTextNode(v)
+    )
   })
 
   var last = first + arr.length
@@ -207,13 +234,13 @@ function arrayFragment(e, arr) {
       switch (ev.type) {
       case 'unshift':
         forEachReverse(ev.values, function(v) {
-          e.insertBefore(isNode(v) ? v : document.createTextNode(v), e.childNodes[first])
+          e.insertBefore(isNode(v) ? v : doc.createTextNode(v), e.childNodes[first])
           last++
         })
       break
       case 'push':
         forEach(ev.values, function(v) {
-          e.insertBefore(isNode(v) ? v : document.createTextNode(v), e.childNodes[last])
+          e.insertBefore(isNode(v) ? v : doc.createTextNode(v), e.childNodes[last])
           last++
         })
         break
@@ -234,3 +261,10 @@ function arrayFragment(e, arr) {
   }
   return frag
 }
+
+
+var h = context(function (el) { return doc.createElement(el) })
+h.context = context
+if (typeof module === 'object') module.exports = h
+
+export default h
